99ec64cc4d8eebc906fb89dd364368153b3bac2d
MBE <zirax83@gmail.com>
2020-06-12 10:25:32 +0200

    Default

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import cold from "./img/snowflake.png";
+import "./index.css";
+
+function Room() {
+  const [isLit, setLit] = useState(true);
+  const [temperature, setTemperature] = useState(6);
+
+  const brightness = isLit ? "lit" : "dark";
+
+  return (
+    <div className={`room ${brightness}`}>
+      The room is {isLit ? "lit" : "dark"}
+      <br />
+      <button onClick={() => setLit(!isLit)}>flip</button>
+      <br />
+      <button onClick={() => setLit(true)}>Turn the light on!</button>
+      <br />
+      <button onClick={() => setLit(false)}>Turn the light off</button>
+      <br />
+      <h1>{temperature}</h1>
+      <br />
+      <OnButton
+        setTemperature={setTemperature}
+        temperature={temperature}
+      ></OnButton>
+      <ImageHandler temperature={temperature}></ImageHandler>
+      <br />
+      <OffButton
+        setTemperature={setTemperature}
+        temperature={temperature}
+      ></OffButton>
+    </div>
+  );
+}
+
+function OnButton({ setTemperature, temperature }) {
+  return <button onClick={() => setTemperature((temperature += 1))}>+</button>;
+}
+
+function OffButton({ setTemperature, temperature }) {
+  return <button onClick={() => setTemperature((temperature -= 1))}>-</button>;
+}
+
+function ImageHandler({ temperature }) {
+  let imageUrl = null;
+
+  if (temperature <= 5) {
+    imageUrl = { cold };
+  }
+
+  return <img src={imageUrl}></img>;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div className="room">
+    <Room />
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
